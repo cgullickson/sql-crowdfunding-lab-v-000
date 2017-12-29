7dfdf4ababf0857d82948cb7373498f1e905df9a
Chris Gullickson <cgullickson5@gmail.com>
2017-12-29 22:38:27 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT DISTINCT projects.title
-SUM pledges.amount
+SUM(pledges.amount)
 FROM projects
 INNER JOIN pledges
 ON projects.id = pledges.project_id
