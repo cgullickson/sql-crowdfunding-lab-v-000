b2dbe657c51d7f08e6aa7787bb689da6bfd260fe
Chris Gullickson <cgullickson5@gmail.com>
2017-12-29 23:11:05 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,12 +36,12 @@ ORDER BY projects.title"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, pledges.amount
+"SELECT users.name, SUM(pledges.amount) as total_amt
 FROM users
 INNER JOIN pledges
 ON users.id = pledges.user_id
 GROUP BY users.name
-ORDER BY pledges.amount, users.name"
+ORDER BY total_amt, users.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
